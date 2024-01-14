df827d385b4d421e150df40e2b0a5c603795dad9
Domenico <domenicoribaudo06@gmail.com>
2024-01-14 17:37:48 +0100

    Commit finale, esercizio completato

--- src/MainTest.java
+++ src/MainTest.java
@@ -11,4 +11,22 @@ class MainTest {
     void stampaFullNonFunziona() {
         assertEquals("venerdì 1 marzo 2002", "venerdì 1 marzo 2002, stringa cambiata");
     }
+    @org.junit.jupiter.api.Test
+    void stampaMediumFunziona() {
+        assertEquals("1 mar 2002", "1 mar 2002");
+    }
+
+    @org.junit.jupiter.api.Test
+    void stampaMediumNonFunziona() {
+        assertEquals("1 mar 2002", "1 mar 2003");
+    }
+    @org.junit.jupiter.api.Test
+    void stampaShortFunziona() {
+        assertEquals("01/03/02", "01/03/02");
+    }
+
+    @org.junit.jupiter.api.Test
+    void stampaShortNonFunziona() {
+        assertEquals("01/03/02", "01/03/03");
+    }
 }
